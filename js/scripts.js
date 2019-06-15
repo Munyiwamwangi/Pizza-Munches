c3fc3548f2807eeb166f26ad994d51b43ee4cad0
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-15 11:05:43 +0300

    created the function to take output of selecting largePizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-function getValueforlarge(){
-  
-}
+$(document).ready(function(){
+  function getValueforlarge(){
+    var initiateLarge=document.getElementsByClassName('largePizza');
+    var space='';
+    for(i=0; i<3; i++){
+      if(largePizza[i].checked===true){
+        space += largePizza[i].value+ ''
+      }
+    }
+})
+alert(space);
