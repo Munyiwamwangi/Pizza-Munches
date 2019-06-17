1b5ad43c51b9348aa00cb1a3de9345cdca2fa70f
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-17 15:15:06 +0300

    creating conditional function to check selected crust and toppings abd adding them

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,42 @@
 //constructors for collecting user input
-function pizzaSize(small, medium, large){
-  this.pizzaSmall=small;
-  this.pizzaMedium=medium;
-  this.pizzaLarge=large;
+function pizzaSize(small, medium, large) {
+  this.pizzaSmall = small;
+  this.pizzaMedium = medium;
+  this.pizzaLarge = large;
 }
 
-function crustType(cripsy,stuffed,gluttenFree){
-  this.pizzaCripsy=cripsy;
-  this.pizzaStuffed=stuffed;
-  this.pizzaGlutten_free=glutenFree;
+function crustType(cripsy, stuffed, gluttenFree) {
+  this.pizzaCripsy = cripsy;
+  this.pizzaStuffed = stuffed;
+  this.pizzaGlutten_free = glutenFree;
 }
-// Hide on click
+/*************************Hide on click***************/
 $(document).ready(function() {
   $("#one").click(function() {
-    $("#hideMedium").hide();
-    $("#hideSmall").hide();
-    alert(largePizza[2])
-    });
+    $("#hideMedium").hide(200);
+    $("#hideSmall").hide(200);
   });
 
-  $(document).ready(function() {
-    $("#two").click(function() {
-      $("#hideSmall").hide();
-      $("#hideLarge").hide();
-      });
-    });
+  $("#two").click(function() {
+    $("#hideSmall").hide(200);
+    $("#hideLarge").hide(200);
+  });
 
-    $(document).ready(function() {
-      $("#three").click(function() {
-        $("#hideLarge").hide();
-        $("#hideMedium").hide();
-        });
-      });
+  $("#three").click(function() {
+    $("#hideLarge").hide(200);
+    $("#hideMedium").hide(200);
+  });
+  event.preventDefault();
+});
 
-var priceSize=[800, 500, 400];
-var priceCrust=[100, 100, 100];
-var priceTopping=[70, 70 ,70];
+var priceSize = [800, 500, 400];
+var priceCrust = [100, 100, 100];
+var priceTopping = [70, 70, 70];
+
+$(document).ready(function(){
+  if($("#one").click() && $("#cripsy").click() && $("input[name= four]:checked"))
+  {
+    var selectedTotal=priceSize[0]+priceCrust[0];
+    return selectedTotal;
+  }
+}
