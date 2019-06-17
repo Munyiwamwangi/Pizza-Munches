68c5fa68a6ae52a3b132ee2f082e371dcb812506
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-17 15:57:38 +0300

    buttons to confirm delivery

--- js/scripts.js
+++ js/scripts.js
@@ -15,28 +15,26 @@ $(document).ready(function() {
   $("#one").click(function() {
     $("#hideMedium").hide(200);
     $("#hideSmall").hide(200);
+      event.preventDefault();
   });
 
   $("#two").click(function() {
     $("#hideSmall").hide(200);
     $("#hideLarge").hide(200);
+      event.preventDefault();
   });
 
   $("#three").click(function() {
     $("#hideLarge").hide(200);
     $("#hideMedium").hide(200);
+      event.preventDefault();
   });
-  event.preventDefault();
 });
 
 var priceSize = [800, 500, 400];
 var priceCrust = [100, 100, 100];
 var priceTopping = [70, 70, 70];
 
-$(document).ready(function(){
-  if($("#one").click() && $("#cripsy").click() && $("input[name= four]:checked"))
-  {
-    var selectedTotal=priceSize[0]+priceCrust[0];
-    return selectedTotal;
-  }
+function myFunction() {
+  confirm("click okay to confirm delivery!");
 }
