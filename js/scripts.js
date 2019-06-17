7062accbd9ed5eb090016972777b4569158a30f7
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-17 08:50:59 +0300

    trying giving user frrdback about progress their order

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
-  function getValueforlarge(){
-    var initiateLarge=document.getElementsByClassName('largePizza');
-    var space='';
-    for(i=0; i<3; i++){
-      if(largePizza[i].checked===true){
-        space += largePizza[i].value+ ''
-      }
-    }
-})
-alert(space);
+    $("#largeSelected").change(function(){
+        var selectedPizza = $(this).children("option#largeSelected").val();
+        alert("Cost " + selectedPizza);
+    });
+});
+
+// $(document).ready(function() {
+//   $(".largeSelected").select(function() {
+//     $(".toppingsSelectorMedium").hide();
+//     $(".toppingsSelectorSmall").hide();
+// });
+// });
